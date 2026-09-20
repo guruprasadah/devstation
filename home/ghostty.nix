1e77cb01f889d84b570b4ed5d0e66aeaf414b830
Guruprasad AH <guruprasadah08@gmail.com>
2026-09-20 09:29:59 +0530

    add logic for ghostty package selection based on platform

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -2,7 +2,7 @@
 {
   programs.ghostty = {
     enable = true;
-    package = pkgs.ghostty-bin;
+    package = if pkgs.stdenv.isDarwin then pkgs.ghostty-bin else pkgs.ghostty;
     enableZshIntegration = true;
   };
 }
